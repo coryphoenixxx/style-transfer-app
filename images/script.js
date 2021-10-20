94f0bbb22f271aa1f613440d53849cee6e4f2a0a
coryphoenixxx <coryphoenixxx@gmail.com>
2021-10-20 21:05:21 +0700

    *improve frontend

--- images/script.js
+++ images/script.js
@@ -1,67 +1,128 @@
-let styles_container = document.body.querySelector("#styles_container")
-
-
-// function makeFileFromUrl(url, filename) {
-//     fetch(url)
-//                     .then(r => r.blob())
-//                     .then(blobFile => new File([blobFile], filename, { type: "image/jpeg" }))
-//                     .then(file => {return file;})
-// }
+"use strict";
 
 function urlToFile(url, filename){
         return (fetch(url)
             .then(function(res){return res.arrayBuffer();})
-            .then(function(buf){return new File([buf], filename,{type: 'image/jpeg'});})
+            .then(function(buf){
+                return new File([buf], filename,{type: 'image/jpeg'});})
         );
     }
 
-
-for (let i = 1; i <= 20; i++) {
-    let style_box = document.createElement('div');
-    style_box.classList.add('style_box');
-    let style_img_url = `images/default_style/default_style_${i}.jpg`;
-    style_box.style.backgroundImage = `url(${ style_img_url })`;
-
-
-    style_box.addEventListener("click", e => {
-        let sel_style = document.getElementById('sel_style');
-
-        let filename = `default_style_${i}.jpg`
-        urlToFile(style_img_url, filename)
-            .then(function(file) {updateThumbnail(sel_style, file)})
-    })
-    styles_container.appendChild(style_box)
-}
-
-
 function download(url) {
-    let thumbnailElements = document.body.querySelectorAll(".drop-zone__thumb")
-    let contentFilename = thumbnailElements[0].dataset.label
-
-    let link = document.createElement('a');
+    const thumbnailElements = document.body.querySelectorAll(".drop-zone__thumb")
+    const contentFilename = thumbnailElements[0].dataset.label
+    const link = document.createElement('a');
     link.href = url;
     link.download = contentFilename.split('.')[0] + '_stylized.jpg'
     document.body.appendChild(link);
     link.click();
-    console.log('CLICK')
     link.remove()
 }
-
-
 
 function getBackgroundImageUrl(element) {
     const backgroundImage = element.style.backgroundImage;
     return backgroundImage.slice(4, -1).replace(/"/g, "");
 }
 
+// Create selection style images box
+const styles_box = document.body.querySelector("#styles-box")
+for (let i=1; i<=20; i++) {
+    const style_elem = document.createElement('div');
+    const style_img_url = `images/default_style/default_style_${i}.jpg`;
+    style_elem.classList.add('style-elem');
+    style_elem.style.backgroundImage = `url(${ style_img_url })`;
+
+    style_elem.addEventListener("click", e => {
+        const style_zone = document.getElementById('style-drop-zone');
+        const filename = `default_style_${i}.jpg`
+        urlToFile(style_img_url, filename)
+            .then(function(file) {updateThumbnail(style_zone, file)})
+    })
+    styles_box.appendChild(style_elem)
+}
+
+styles_box.addEventListener("wheel", e => {
+         e.preventDefault();
+         styles_box.scrollLeft += e.deltaY;
+    })
 
 
+
+
+document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+    const promptElement = dropZoneElement.querySelector(".drop-zone__prompt");
+
+    dropZoneElement.addEventListener("click", e => {
+        inputElement.click()
+    })
+
+    inputElement.addEventListener("change", e => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+        inputElement.value = ''
+    })
+
+    dropZoneElement.addEventListener("dragover", e => {
+        e.preventDefault()
+        promptElement.classList.add("drop-zone__prompt--over");
+        // dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach(type => {
+        dropZoneElement.addEventListener(type, e => {
+            promptElement.classList.remove("drop-zone__prompt--over");
+            // dropZoneElement.classList.remove("drop-zone--over");
+        })
+    })
+
+    dropZoneElement.addEventListener("drop", e => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        promptElement.classList.remove("drop-zone__prompt--over");
+    });
+});
+
+function updateThumbnail(dropZoneElement, file) {
+    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+    // First time - remove the prompt
+    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+        dropZoneElement.querySelector(".drop-zone__prompt").remove();
+    }
+
+    // First time - there is no thumbnail element, so let's create it
+    if (!thumbnailElement) {
+        thumbnailElement = document.createElement("div");
+        thumbnailElement.classList.add("drop-zone__thumb");
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    // Show thumbnail for image files
+
+    if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = () => {
+            let image = new Image();
+            image.src = reader.result;
+            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+        };
+    } else {
+        thumbnailElement.style.backgroundImage = null;
+    }
+}
 
 async function sendRequest() {
-    // const content = document.getElementById('content').files[0];
-    // const style = document.getElementById('style').files[0];
-
-
     let Thumbs = document.body.querySelectorAll('.drop-zone__thumb')
     let contentImageUrl = getBackgroundImageUrl(Thumbs[0])
     let styleImageUrl = getBackgroundImageUrl(Thumbs[1])
@@ -73,7 +134,7 @@ async function sendRequest() {
 
     // if (typeof content == "undefined" || typeof style == "undefined") return;
 
-    let stylized = document.getElementById('stylized');
+    let stylized = document.getElementById('stylized-output-zone');
     let thumb = stylized.querySelector('.drop-zone__thumb')
 
     if (stylized.querySelector(".drop-zone__prompt")) {
@@ -106,86 +167,10 @@ async function sendRequest() {
             thumb.style.backgroundImage = `url('${ imageUrl }')`;
             thumb.dataset.label = 'CLICK TO DOWNLOAD';
 
-
             thumb.addEventListener('click', e => {
                 download(imageUrl);
             })
         })
     })
-}
-
-
-document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
-    const dropZoneElement = inputElement.closest(".drop-zone");
-
-    dropZoneElement.addEventListener("click", e => {
-        inputElement.click()
-    })
-
-    inputElement.addEventListener("change", e => {
-        if (inputElement.files.length) {
-            updateThumbnail(dropZoneElement, inputElement.files[0]);
-        }
-    })
-
-    dropZoneElement.addEventListener("dragover", e => {
-        e.preventDefault()
-        dropZoneElement.classList.add("drop-zone--over");
-    });
-
-    ["dragleave", "dragend"].forEach(type => {
-        dropZoneElement.addEventListener(type, e => {
-            dropZoneElement.classList.remove("drop-zone--over");
-        })
-    })
-
-    dropZoneElement.addEventListener("drop", e => {
-        e.preventDefault();
-
-        if (e.dataTransfer.files.length) {
-            inputElement.files = e.dataTransfer.files;
-            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
-        }
-
-        dropZoneElement.classList.remove("drop-zone--over");
-    });
-});
-
-function updateThumbnail(dropZoneElement, file) {
-
-    const style = document.getElementById('style');
-
-
-    let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
-
-    // First time - remove the prompt
-    if (dropZoneElement.querySelector(".drop-zone__prompt")) {
-        dropZoneElement.querySelector(".drop-zone__prompt").remove();
-    }
-
-    // First time - there is no thumbnail element, so let's create it
-    if (!thumbnailElement) {
-        thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb");
-        dropZoneElement.appendChild(thumbnailElement);
-    }
-
-    thumbnailElement.dataset.label = file.name;
-
-    // Show thumbnail for image files
-
-    if (file.type.startsWith('image/')) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-
-        reader.onload = () => {
-            let image = new Image();
-            image.src = reader.result;
-            thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
-        };
-    } else {
-        thumbnailElement.style.backgroundImage = null;
-    }
-
 }
 
